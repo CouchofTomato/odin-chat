35c4031cc711d5b9f0731a3eb65f01c15aeec2d2
Austin <austinpmason@gmail.com>
2020-08-25 13:30:17 +0100

    add latest changes

--- spec/support/capybara_support.rb
+++ spec/support/capybara_support.rb
@@ -1,3 +1,14 @@
 Capybara.configure do |config|
   config.test_id = "data-test-id"
 end
+Capybara.register_driver :selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[
+      headless disable-gpu no-sandbox
+      --window-size=1920,1080
+      --enable-features=NetworkService,NetworkServiceInProcess
+    ]
+
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
